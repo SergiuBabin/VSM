523ec572ee4ed3288335a5de3815de18829fa7c4
Sergiu Babin <48562108+SergiuBabin@users.noreply.github.com>
2020-12-26 01:11:35 +0200

    .

--- src/ArrayMap.java
+++ src/ArrayMap.java
@@ -1,7 +1,4 @@
-import java.util.AbstractMap;
-import java.util.ArrayList;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class ArrayMap<K, V> extends AbstractMap<K, V> {
 
@@ -109,6 +106,68 @@ public class ArrayMap<K, V> extends AbstractMap<K, V> {
 
     @Override
     public Set<Entry<K, V>> entrySet() {
-        return null;
+        if (entries == null) {
+            entries = new AbstractSet<>() {
+                public void clear() {
+                    list.clear();
+                }
+                @Override
+                public Iterator iterator() {
+                    return list.iterator();
+                }
+
+                @Override
+                public int size() {
+                    return list.size();
+                }
+            };
+        }
+        return entries;
     }
+
+    public String toString() {
+        return list.toString();
+    }
+}
+
+class Prob1 {
+    public static void main(String args[]) {
+        ArrayMap<Integer, String> map = new ArrayMap<>();
+        map.put(1, "aaa");
+        map.put(2, "saa");
+        map.put(3, "saas");
+        System.out.println("Verificam...");
+        System.out.println("Continutul colectiei: " + map);
+        if (map.size() != 3) {
+            System.err.println("ArrayMap.size() (" + map.size() + ") a fost implementata gresit.");
+        }
+
+        Set<Map.Entry<Integer, String>> entries = map.entrySet();
+        if (entries.size() != 3) {
+            System.err.println("ArrayMap.entrySet() a fost implementata gresit.");
+        }
+
+        for (Map.Entry<Integer, String> e : entries) {
+            map.put(e.getKey(), new StringBuffer(e.getValue()).reverse().toString());
+            if (!map.entrySet().contains(e)) {
+                System.err.println("ArrayMap.put() nu inlocuieste vechea valoare.");
+            }
+            if ((!e.toString().contains(e.getKey().toString()) || (!e.toString().contains(e.getValue())))) {
+                System.err.println("ArrayMap.ArrayMapEntry.toString() a fost implementata gresit.");
+            }
+        }
+
+        for (Map.Entry<Integer, String> e1 : entries) {
+            for (Map.Entry<Integer, String> e2 : entries) {
+                if ((e1 == e2) != (e1.equals(e2))) {
+                    System.err.println("ArrayMap.ArrayMapEntry.equals() a fost implementata gresit.");
+                }
+                if ((e1 == e2) != (e1.hashCode() == e2.hashCode())) {
+                    System.err.println("ArrayMap.ArrayMapEntry.hashCode() a fost implementata gresit.");
+                }
+            }
+        }
+
+    }
+
 }
